fe2583d45762617904038d51252c85585e85d947
akash100a <arakash968786@gmail.com>
2025-03-05 02:13:40 +0600

    update quiz page loading time to 4 seconds; refactor layout structure and add loading component

---
+++ app/(pages)/parallerdashboard/@quiz/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center h-full bg-lime-200">
+      <div className="text-center">
+        <p className="text-xl font-bold text-gray-700">Loading Quiz 4s Please...</p>
+      </div>
+    </div>
+  )
+}
+
+export default loading
--- app/(pages)/parallerdashboard/layout.jsx
+++ app/(pages)/parallerdashboard/layout.jsx
@@ -1,19 +1,16 @@
-import Assignment from "@/app/components/Assignment";
-import Quiz from "@/app/components/Quiz";
-import Notification from "@/app/components/Notification";
-
-const Layout = ({ children,assignment,notification,quiz}) => {
+const Layout = ({ children, assignment, notification, quiz }) => {
   return (
     <>
-    <div className="grid grid-rows-2 grid-cols-2 h-screen">
-      {assignment}
-      {quiz}
-      {notification}
-    </div>
+      <div className="grid grid-rows-2 h-screen">
+        <div className="grid grid-cols-2">
+          {assignment}
+          {notification}
+        </div>
+        <div className="">{quiz}</div>
+      </div>
       {children}
     </>
   );
 };
 
 export default Layout;
-

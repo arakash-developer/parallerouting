b548ef08f38ab7f65e53a034a4f4c076e630d250
akash100a <arakash968786@gmail.com>
2025-03-05 04:30:08 +0600

    remove redundant height class from loading component in quiz page

--- app/(pages)/parallerdashboard/@quiz/loading.jsx
+++ app/(pages)/parallerdashboard/@quiz/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div className="h-[200px] flex items-center justify-center h-full bg-lime-200">
+    <div className="h-[200px] flex items-center justify-center bg-lime-200">
       <div className="text-center">
         <p className="text-xl font-bold text-gray-700">Loading QuizPlease...</p>
       </div>

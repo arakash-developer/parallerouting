38df05468754c39e18fd594eb869bf56413c8449
akash100a <arakash968786@gmail.com>
2025-03-05 04:38:20 +0600

    refactor notification page and update ESLint configuration for console warnings

--- app/(pages)/parallerdashboard/@notification/page.jsx
+++ app/(pages)/parallerdashboard/@notification/page.jsx
@@ -4,7 +4,7 @@ const page = async () => {
   await wait(3000);
   return (
     <div
-      className="bg-gradient-to-r from-blue-100 to-blue-200 border-t-4 border-blue-500 text-blue-700 px-8 py-6 rounded-lg shadow-md"
+      className="bg-gradient-to-r from-blue-100 to-blue-200 text-blue-700 px-8 py-6 rounded-lg shadow-md"
       role="alert"
     >
       <h2 className="font-bold text-2xl mb-4">Notification</h2>
